10f30da74de763a684e528fd3bec837b94fd48ec
CN-Root <admin@root.tax>
2022-08-16 17:29:58 +0800

    项目文件
    项目文件

---
+++ duokai/Home.Designer.cs
@@ -0,0 +1,255 @@
+﻿
+namespace shuangkai
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
+            this.copyright = new System.Windows.Forms.Label();
+            this.rootlink = new System.Windows.Forms.LinkLabel();
+            this.time_nian = new System.Windows.Forms.Label();
+            this.banquan = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.wechat_shuangkai = new System.Windows.Forms.PictureBox();
+            this.wework_shuangkai = new System.Windows.Forms.PictureBox();
+            this.wechat_path = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.wechat_time = new System.Windows.Forms.Timer(this.components);
+            this.wework_time = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.banquan.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.wechat_shuangkai)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wework_shuangkai)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // copyright
+            // 
+            this.copyright.AutoSize = true;
+            this.copyright.Font = new System.Drawing.Font("微软雅黑", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.copyright.Location = new System.Drawing.Point(44, 2);
+            this.copyright.Name = "copyright";
+            this.copyright.Size = new System.Drawing.Size(96, 20);
+            this.copyright.TabIndex = 3;
+            this.copyright.Text = "Copyright © ";
+            // 
+            // rootlink
+            // 
+            this.rootlink.AutoSize = true;
+            this.rootlink.Font = new System.Drawing.Font("微软雅黑", 10F);
+            this.rootlink.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
+            this.rootlink.Location = new System.Drawing.Point(132, 2);
+            this.rootlink.Name = "rootlink";
+            this.rootlink.Size = new System.Drawing.Size(112, 20);
+            this.rootlink.TabIndex = 4;
+            this.rootlink.TabStop = true;
+            this.rootlink.Text = "root.tax(根目录)";
+            this.rootlink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.rootlink_LinkClicked);
+            // 
+            // time_nian
+            // 
+            this.time_nian.AutoSize = true;
+            this.time_nian.Font = new System.Drawing.Font("微软雅黑", 10F);
+            this.time_nian.Location = new System.Drawing.Point(239, 2);
+            this.time_nian.Name = "time_nian";
+            this.time_nian.Size = new System.Drawing.Size(41, 20);
+            this.time_nian.TabIndex = 5;
+            this.time_nian.Text = "1970";
+            // 
+            // banquan
+            // 
+            this.banquan.Controls.Add(this.rootlink);
+            this.banquan.Controls.Add(this.copyright);
+            this.banquan.Controls.Add(this.time_nian);
+            this.banquan.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.banquan.Location = new System.Drawing.Point(0, 148);
+            this.banquan.Name = "banquan";
+            this.banquan.Size = new System.Drawing.Size(337, 25);
+            this.banquan.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("微软雅黑", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(28, 26);
+            this.textBox1.MaxLength = 2;
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(90, 60);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "2";
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // wechat_shuangkai
+            // 
+            this.wechat_shuangkai.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.wechat_shuangkai.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.wechat_shuangkai.Image = global::duokai.Properties.Resources.WeChat;
+            this.wechat_shuangkai.Location = new System.Drawing.Point(124, 26);
+            this.wechat_shuangkai.Name = "wechat_shuangkai";
+            this.wechat_shuangkai.Size = new System.Drawing.Size(90, 60);
+            this.wechat_shuangkai.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.wechat_shuangkai.TabIndex = 8;
+            this.wechat_shuangkai.TabStop = false;
+            this.wechat_shuangkai.Click += new System.EventHandler(this.wechat_shuangkai_Click);
+            // 
+            // wework_shuangkai
+            // 
+            this.wework_shuangkai.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.wework_shuangkai.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.wework_shuangkai.Image = global::duokai.Properties.Resources.WXWork;
+            this.wework_shuangkai.Location = new System.Drawing.Point(220, 26);
+            this.wework_shuangkai.Name = "wework_shuangkai";
+            this.wework_shuangkai.Size = new System.Drawing.Size(90, 60);
+            this.wework_shuangkai.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.wework_shuangkai.TabIndex = 9;
+            this.wework_shuangkai.TabStop = false;
+            this.wework_shuangkai.Click += new System.EventHandler(this.wework_shuangkai_Click);
+            // 
+            // wechat_path
+            // 
+            this.wechat_path.AutoSize = true;
+            this.wechat_path.Font = new System.Drawing.Font("微软雅黑", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.wechat_path.Location = new System.Drawing.Point(5, 95);
+            this.wechat_path.Name = "wechat_path";
+            this.wechat_path.Size = new System.Drawing.Size(79, 20);
+            this.wechat_path.TabIndex = 10;
+            this.wechat_path.Text = "微信路径：";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(5, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 20);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "企微路径：";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Location = new System.Drawing.Point(74, 94);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(254, 23);
+            this.textBox2.TabIndex = 12;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox3.Location = new System.Drawing.Point(74, 120);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(254, 23);
+            this.textBox3.TabIndex = 13;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(5, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 80);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "双\r\n开\r\n数\r\n量";
+            // 
+            // wechat_time
+            // 
+            this.wechat_time.Interval = 1;
+            this.wechat_time.Tick += new System.EventHandler(this.wechat_time_Tick);
+            // 
+            // wework_time
+            // 
+            this.wework_time.Interval = 1;
+            this.wework_time.Tick += new System.EventHandler(this.wework_time_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(71, 4);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(194, 17);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "点击图标后切莫着急,让子弹飞一会.";
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(337, 173);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.wechat_path);
+            this.Controls.Add(this.wework_shuangkai);
+            this.Controls.Add(this.wechat_shuangkai);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.banquan);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Home";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "微信-企业微信双开神器";
+            this.Load += new System.EventHandler(this.Home_Load);
+            this.banquan.ResumeLayout(false);
+            this.banquan.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.wechat_shuangkai)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wework_shuangkai)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label copyright;
+        private System.Windows.Forms.LinkLabel rootlink;
+        private System.Windows.Forms.Label time_nian;
+        private System.Windows.Forms.Panel banquan;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox wechat_shuangkai;
+        private System.Windows.Forms.PictureBox wework_shuangkai;
+        private System.Windows.Forms.Label wechat_path;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer wechat_time;
+        private System.Windows.Forms.Timer wework_time;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ duokai/Home.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace shuangkai
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        #region 定义
+        string jisuanjishijian = DateTime.Now.ToLocalTime().ToString();//检测当前电脑时间
+        #endregion
+
+        #region 判断微信和企业微信是否安装
+        public void wechat_wework() 
+        {
+            #region 判断微信是否安装
+            Object wechat = Registry.GetValue(@"HKEY_CURRENT_USER\SOFTWARE\Tencent\WeChat", "InstallPath", null);
+            if (wechat != null)
+            {//注册表键值存在
+                
+            }
+            else
+            {//注册表键值不存在
+                wechat_shuangkai.Hide();
+                MessageBox.Show("未安装微信,因此该功能暂不可用", "提示：");
+            }  
+            #endregion
+
+            #region 判断企微是否安装
+            Object wework = Registry.GetValue(@"HKEY_CURRENT_USER\SOFTWARE\Tencent\WXWork", "Executable", null);
+            if (wework != null)
+            {//注册表键值存在
+                
+            }
+            else
+            {//注册表键值不存在
+                wework_shuangkai.Hide();
+                MessageBox.Show("未安装企微,因此该功能暂不可用", "提示：");
+            }
+            #endregion
+        }
+        #endregion
+
+        #region 修改企业微信双开注册表
+        string qywxsk = "0";
+        public void xgqywxzcb() 
+        {
+            #region 修改企业微信双开注册表
+            string strNum1 = textBox1.Text;
+            string strNum2 = "3";
+            float flNum1 = Convert.ToSingle(strNum1);
+            float flNum2 = Convert.ToSingle(strNum2);
+            float sum = flNum1 + flNum2;
+            qywxsk = Convert.ToString(sum);
+
+            RegistryKey key = Registry.CurrentUser;
+            RegistryKey software = key.OpenSubKey("SOFTWARE\\Tencent\\WXWork", true); //该项必须已存在
+            software.SetValue("multi_instances", qywxsk);
+            //HKEY_CURRENT_USER\SOFTWARE\Tencent\WXWork下创建一个名为“multi_instances”，值为“3”的键值。
+            //如果该键值原本已经存在，则会修改替换原来的键值，如果不存在则是创建该键值。
+            // 注意：SetValue()还有第三个参数，主要是用于设置键值的类型，如：字符串，二进制，Dword等等~~默认是字符串。如：
+            // software.SetValue("test", "0", RegistryValueKind.DWord); //二进制信息
+            key.Close();
+            #endregion
+        }
+        #endregion
+
+        #region 加载主页
+        private void Home_Load(object sender, EventArgs e)
+        {
+            #region 判断微信和企微是否安装
+            wechat_wework();
+            #endregion
+
+            #region 控件居中
+            base.OnResize(e);//控件居中
+            int x1 = (int)(0.5 * (this.Width - banquan.Width));
+            int y1 = banquan.Location.Y;
+            banquan.Location = new System.Drawing.Point(x1, y1);
+            #endregion
+
+            #region 获取年份
+            string nianfen = jisuanjishijian.Substring(0,4);//截取字符串前4位字符
+            time_nian.Text = nianfen;
+            #endregion
+
+            #region 读取企微安装路径
+            RegistryKey registryKeywework = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Tencent\WXWork");
+            string wework_path = (string)registryKeywework.GetValue("Executable".ToUpper());
+            textBox3.Text = wework_path;
+            #endregion
+
+            #region 读取微信安装路径
+            RegistryKey registryKeywechat = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Tencent\WeChat");
+            string wechat_path = (string)registryKeywechat.GetValue("InstallPath".ToUpper());
+            textBox2.Text = wechat_path + "\\WeChat.exe";
+            #endregion
+        }
+        #endregion
+
+        #region 前往官网
+        private void rootlink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://www.root.tax/", "Open web site");  //打开网站
+        }
+        #endregion
+
+        #region 限制textbox只能输入数字,且首字不能为0
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar != '\b')//这是允许输入退格键 
+            {
+                int len = textBox1.Text.Length;
+                if (len < 1 && e.KeyChar == '0')
+                {
+                    e.Handled = true;
+                }
+                else if ((e.KeyChar < '0') || (e.KeyChar > '9'))//这是允许输入0-9数字 
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+        #endregion
+
+        #region 多开
+        string chat_shu = "0";
+        private void wechat_shuangkai_Click(object sender, EventArgs e)
+        {//微信
+            #region 判断微信是否安装,如安装,及执行多开
+            Object obj = Registry.GetValue(@"HKEY_CURRENT_USER\SOFTWARE\Tencent\WeChat", "InstallPath", null);
+            if (obj != null)
+            {//注册表键值存在
+                wechat_time.Enabled = true;//开启计时器
+            }
+            else
+            {//注册表键值不存在
+                MessageBox.Show("未安装微信,因此该功能暂不可用", "提示：");
+            }
+            #endregion
+        }
+
+        string work_shu = "0";
+        private void wework_shuangkai_Click(object sender, EventArgs e)
+        {//企业微信
+            xgqywxzcb();//修改企业微信双开注册表
+
+            #region 判断企微是否安装,如安装,及执行多开
+            Object obj = Registry.GetValue(@"HKEY_CURRENT_USER\SOFTWARE\Tencent\WXWork", "Executable", null);
+            if (obj != null)
+            {//注册表键值存在
+                wework_time.Enabled = true;//开启计时器
+            }
+            else
+            {//注册表键值不存在
+                MessageBox.Show("未安装企微,因此该功能暂不可用", "提示：");
+            }
+            #endregion
+        }
+        #endregion
+
+        #region 循环开启微信&企业微信
+        string jieguo = "";
+        private void wechat_time_Tick(object sender, EventArgs e)
+        {//循环开启微信
+            if (chat_shu == textBox1.Text)
+            {
+                wechat_time.Enabled = false;//关闭计时器
+                chat_shu = "0";
+            }
+            else
+            {
+                string strNum1 = chat_shu;
+                string strNum2 = "1";
+                float flNum1 = Convert.ToSingle(strNum1);
+                float flNum2 = Convert.ToSingle(strNum2);
+                float sum = flNum1 + flNum2;
+                chat_shu = Convert.ToString(sum);
+
+                Process.Start(@textBox2.Text, "Open program");//打开程序 
+            }
+        }
+
+        private void wework_time_Tick(object sender, EventArgs e)
+        {//循环开启企业微信
+            xgqywxzcb();//修改企业微信双开注册表
+
+            if (work_shu == textBox1.Text)
+            {
+                wework_time.Enabled = false;//关闭计时器
+                work_shu = "0";
+            }
+            else 
+            {
+                string strNum1 = work_shu;
+                string strNum2 = "1";
+                float flNum1 = Convert.ToSingle(strNum1);
+                float flNum2 = Convert.ToSingle(strNum2);
+                float sum = flNum1 + flNum2;
+                work_shu = Convert.ToString(sum);
+
+                Process.Start(@textBox3.Text, "Open program");//打开程序 
+            }
+        }
+        #endregion
+    }
+}
